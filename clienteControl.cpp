9900910ff7b1594bd60936726932289482eef80a
camilomedina88 <ceaiotcamm@outlook.com>
2016-11-14 21:36:22 -0500

    Update clienteControl.cpp

--- clienteControl.cpp
+++ clienteControl.cpp
@@ -8,6 +8,8 @@
 #include <string.h>
 #include <mosquitto.h>
 #include "mraa.hpp"
+#include <pthread.h>
+#include <string.h>
 
 // Server connection parameters
 #define MQTT_HOSTNAME "nickiler.ddns.net"
@@ -15,57 +17,83 @@
 #define MQTT_USERNAME ""
 #define MQTT_PASSWORD ""
 #define MQTT_TOPIC "hello"
+#define IDVAR_BOMBA "/solution/fad8397a-c934-48fa-8aee-7e6e4c2f5fd2/5a1bf940-3636-4223-8fc5-8f76bfccaae8/lv"
+#define IDVAR_LEDS "/solution/fad8397a-c934-48fa-8aee-7e6e4c2f5fd2/a7b77b27-b5dc-4154-84e4-5810c9fc2928/lv"
+#define IDVAR_VENTILADOR "/solution/fad8397a-c934-48fa-8aee-7e6e4c2f5fd2/44794d3e-b9e1-4f48-8fb9-2abf59270811/lv"
+
 using namespace std;
 char* estadoBomba;
 int accionBomba, accionLeds, accionVentilador;
 mraa::Gpio* bomba = new mraa::Gpio(8, true, false);
 mraa::Gpio* leds = new mraa::Gpio(3, true, false);
 mraa::Gpio* ventilador = new mraa::Gpio(7, true, false);
+struct mosquitto *mosq1 = NULL;
+struct mosquitto *mosq2 = NULL;
+struct mosquitto *mosq3 = NULL;
+char *host = "nickiler.ddns.net";
+int port = 1883;
+int keepalive = 60;
+bool clean_session = true;
 
-void deserialize(char * mensajeOriginal) {
+void deserialize(int topic, char * mensajeOriginal) {
 
-	sscanf(mensajeOriginal,
-			"[{\"name\":\"bomba\",\"v\":%d},{\"name\":\"leds\",\"v\":%d},{\"name\":\"ventilador\",\"v\":%d}]",
-			&accionBomba, &accionLeds, &accionVentilador);
-	cout << "Valor Bomba: " << accionBomba << endl;
-	cout << "Valor Leds: " << accionLeds << endl;
-	cout << "Valor ventilador: " << accionVentilador << endl;
+	if (topic == 1) {
+		sscanf(mensajeOriginal, "[{\"name\":\"bomba\",\"v\":%d}]",
+				&accionBomba);
+		cout << "Valor Bomba: " << accionBomba << endl;
+	}
+
+	//sscanf(mensajeOriginal,
+	//	"[{\"name\":\"bomba\",\"v\":%d},{\"name\":\"leds\",\"v\":%d},{\"name\":\"ventilador\",\"v\":%d}]",
+	//&accionBomba, &accionLeds, &accionVentilador);
+
+	//cout << "Valor Leds: " << accionLeds << endl;
+	//cout << "Valor ventilador: " << accionVentilador << endl;
 }
 
 void my_message_callback(struct mosquitto *mosq, void *userdata,
 		const struct mosquitto_message *message) {
 	if (message->payloadlen) {
-		printf("%s %s\n", message->topic, message->payload);
-		estadoBomba = (char*) message->payload;
-		deserialize(estadoBomba);
-		if (accionBomba == 1) {
-			bomba->write(1);
-			cout << "Prendio Bomba" << endl;
-		}
-		if (accionBomba == 0) {
-			bomba->write(0);
-			cout << "Apago Bomba" << endl;
+		//printf("%s %s\n", message->topic, message->payload);
+
+		string msjRecibido = (string) message->topic;
+		//int comparacionTest = msjRecibido.compare(IDVAR_BOMBA);
+		//cout << "Comparando este es:  " << comparacionTest << endl;
+
+		int esBomba =msjRecibido.compare(IDVAR_BOMBA) ;
+		int esVentilador =msjRecibido.compare(IDVAR_VENTILADOR) ;
+		int esLeds = msjRecibido.compare(IDVAR_LEDS);
+/*
+		int esBomba = strncmp(IDVAR_BOMBA, message->topic, 80);
+		int esVentilador = strncmp(IDVAR_LEDS, message->topic, 80);
+		int esLeds = strncmp(IDVAR_VENTILADOR, message->topic, 80);
+*/
+		cout << endl << "---" << endl;
+		if (esBomba == 0) {
+			sscanf((char *) message->payload, "%d", &accionBomba);
+			cout << "LA ACCION DE LA BOMBA ES: " << accionBomba << endl;
+			bomba->write(accionBomba);
+
 		}
 
-		if (accionLeds == 1) {
-			leds->write(1);
-			cout << "Prendio Leds" << endl;
+		if (esVentilador == 0) {
+			sscanf((char *) message->payload, "%d", &accionVentilador);
+			cout << "LA ACCION DEL VENTILADOR ES: " << accionVentilador << endl;
+			ventilador->write(accionVentilador);
 		}
 
-		if (accionLeds == 0) {
-			leds->write(0);
-			cout << "Apago Leds" << endl;
+		if (esLeds==0) {
+			sscanf((char *) message->payload, "%d", &accionLeds);
+			cout << "LA ACCION DEL LEDS ES: " << accionLeds << endl;
+			leds->write(accionLeds);
 		}
 
-		if (accionVentilador == 1) {
-			ventilador->write(1);
-			cout<<"Prendio Ventilador"<<endl;
-		}
-		if (accionVentilador == 0) {
-			ventilador->write(0);
-			cout<<"Apago Ventilador"<<endl;
-		}
+		printf("-------------- \n");
 
+
+		esBomba=2;
+		esVentilador=2;
+		esLeds=2;
 	} else {
 		printf("%s (null)\n", message->topic);
 	}
@@ -76,7 +104,10 @@ void my_connect_callback(struct mosquitto *mosq, void *userdata, int result) {
 	int i;
 	if (!result) {
 		/* Subscribe to broker information topics on successful connect. */
-		mosquitto_subscribe(mosq, NULL, "hello", 2);
+
+		mosquitto_subscribe(mosq, NULL, IDVAR_BOMBA, 2);
+		mosquitto_subscribe(mosq, NULL, IDVAR_VENTILADOR, 2);
+		mosquitto_subscribe(mosq, NULL, IDVAR_LEDS, 2);
 	} else {
 		fprintf(stderr, "Connect failed\n");
 	}
@@ -104,7 +135,6 @@ int main(int argc, char **argv) {
 	int port = 1883;
 	int keepalive = 60;
 	bool clean_session = true;
-	struct mosquitto *mosq = NULL;
 
 	//BOMBA
 	//mraa::Gpio* bomba = new mraa::Gpio(8, true, false);
@@ -144,29 +174,27 @@ int main(int argc, char **argv) {
 		return MRAA_ERROR_UNSPECIFIED;
 	}
 
-	sleep(5);
-	cout << "RELE" << endl;
-
-
 	mosquitto_lib_init();
-	mosq = mosquitto_new(NULL, clean_session, NULL);
-	if (!mosq) {
+	mosq1 = mosquitto_new(NULL, clean_session, NULL);
+	if (!mosq1) {
 		fprintf(stderr, "Error: Out of memory.\n");
-		return 1;
+		return 0;
 	}
-	mosquitto_log_callback_set(mosq, my_log_callback);
-	mosquitto_connect_callback_set(mosq, my_connect_callback);
-	mosquitto_message_callback_set(mosq, my_message_callback);
-	mosquitto_subscribe_callback_set(mosq, my_subscribe_callback);
+	mosquitto_log_callback_set(mosq1, my_log_callback);
+	mosquitto_connect_callback_set(mosq1, my_connect_callback);
+	mosquitto_message_callback_set(mosq1, my_message_callback);
+	mosquitto_subscribe_callback_set(mosq1, my_subscribe_callback);
 
-	if (mosquitto_connect(mosq, host, port, keepalive)) {
+	if (mosquitto_connect(mosq1, host, port, keepalive)) {
 		fprintf(stderr, "Unable to connect.\n");
-		return 1;
+		return 0;
 	}
 
-	mosquitto_loop_forever(mosq, -1, 1);
-	mosquitto_destroy(mosq);
+	mosquitto_loop_forever(mosq1, -1, 1);
+	mosquitto_destroy(mosq1);
 	mosquitto_lib_cleanup();
+	return 0;
 
 	return 0;
 }
+
